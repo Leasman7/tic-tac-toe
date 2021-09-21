d3204a47a745086fa770d2020d9f2fb5a9c7ded9
Savannah Leasman <leasman7@gmail.com>
2021-09-21 11:47:56 -0500

    updated readme and added a few divs to the js

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tic-tac-toe
+A two-player game of tic-tac-toe. Player one is "Xs," and Player 2 is "Os"
+
+## How to Run:
+
+
+## Roadmap:
+Working on making the game function
+
+##License:
+This project is licensed under the MIT license as described in included license file.
--- colorGame00.js
+++ colorGame00.js
@@ -1,19 +1,33 @@
+const Square = () => {
+  return (
+    <button></button>
+  )
+}
+
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`;
+  function renderSquare(i) {
+    return <Square></Square>;
+  }
   return (
     <div
       className="game-board"
       onClick={(e) => {
         if (player === 1) {
-        setPlayer(player - 1);
+        setPlayer(player + 1);
         };
-        if (player === 0) {
-          setPlayer(player + 1);
+        if (player === 2) {
+          setPlayer(player - 1);
         };
         status = `Player ${player}`;
       }}
     >
+      <div className="grid-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
       <div id="info">
         <h1>{status}</h1>
       </div>
