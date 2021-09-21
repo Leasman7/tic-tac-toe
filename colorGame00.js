60d9058042039578f4880c846fe27d37b35eb100
Savannah Leasman <leasman7@gmail.com>
2021-09-21 14:28:38 -0500

    clicking changes player and puts 'X' or 'O'

--- colorGame00.js
+++ colorGame00.js
@@ -1,35 +1,58 @@
-const Square = () => {
+const Square = ({ id, player, newState }) => {
+  const [color, setColor] = React.useState('green');
+  const [status, setStatus] = React.useState(null);
+  const xo = ["X", "O"];
+
+  const palet = ['red', 'blue', 'green'];
+  const getRandomColor = () => palet[Math.floor(Math.random()*3)];
+  
+  React.useEffect(() => {
+    console.log(`Render ${id}`)
+    return () => console.log(`unmounting Square ${id}`);
+  })
+  
   return (
-    <button></button>
-  )
-}
+    <button onClick={(e) => {
+      let col = getRandomColor();
+      setColor(col);
+      let nextPlayer = newState({id: id, color: col})
+      setStatus(nextPlayer);
+      e.target.style.background = col;
+    }}> 
+      <h1>{xo[status]}</h1> 
+    </button>
+  );
+};
 
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
+  const [state, setState] = React.useState([]);
+  
   let status = `Player ${player}`;
+  
+  const newState = (obj) => {
+    let nextplayer = (player + 1) % 2;
+    setPlayer(nextplayer);
+    setState([...state, obj]);    
+    console.log(`adding state ${JSON.stringify(state)}`);
+    status = `Player ${nextplayer}`;
+    return nextplayer;
+  };
+
   function renderSquare(i) {
-    return <Square></Square>;
+    return <Square id={i} player={player} newState={newState}></Square>;
   }
   return (
-    <div
-      className="game-board"
-      onClick={(e) => {
-        if (player === 1) {
-        setPlayer(player + 1);
-        };
-        if (player === 2) {
-          setPlayer(player - 1);
-        };
-        status = `Player ${player}`;
-      }}
-    >
+    <div className="game-board">
       <div className="grid-row">
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
       <div id="info">
-        <h1>{status}</h1>
+        <button>Show/HideRow</button>
+        <button>Re-render</button>
+        <h1>Player {player}'s Turn</h1>
       </div>
     </div>
   );
