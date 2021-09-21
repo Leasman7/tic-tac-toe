0bc60e2367c44daaca1f50cc7a6bcc29239d21aa
Savannah Leasman <leasman7@gmail.com>
2021-09-21 15:49:56 -0500

    renamed two files and major changes to js files (not currently working)

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,23 @@
+function checkWinner(state) {
+  const win = [
+      [0, 1, 2]
+      [3, 4, 5]
+      [6, 7, 8]
+      [0, 3, 6]
+      [1, 4, 7]
+      [2, 5, 8]
+      [0, 4, 8]
+      [2, 4, 6]
+  ];
+
+  for (let i = 0; i < win.length; i++) {
+      const [a, b, c] = win[i];
+      if (state[a] == state[b] && state[a] == state[c] && state[a])
+          return state[a];
+  };
+  return null;
+};
+
 const Square = ({ id, player, newState }) => {
   const [color, setColor] = React.useState('green');
   const [status, setStatus] = React.useState(null);
